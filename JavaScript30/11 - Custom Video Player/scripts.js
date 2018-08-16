64ae3caa7d1d08f017441935be01e7442df1dc68
Rich Smith <rs6869@me.com>
2018-08-16 16:53:48 -0400

    Added Project 11 - Custom HTML5 Video Player

---
+++ JavaScript30/11 - Custom Video Player/scripts.js
@@ -0,0 +1,36 @@
+//* Get our elements
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelector('[data-skip]');
+const ranges = player.querySelectorAll('.player__silder');
+
+//* Build out functions
+//* Play and pause buttons
+// '►'  '❚ ❚'
+function togglePlay() {
+    if(video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
+}
+
+function updateButton() {
+    const icon = this.paused ? '►' : '❚ ❚';
+    toggle.textContent = icon;
+    //console.log('Update the button')
+} 
+
+//* Hook up the event listeners
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('paused', updateButton);
+
+toggle.addEventListener('click', togglePlay);
+
+
+// ******************************************* //
+// start video at 10:50 mark 
